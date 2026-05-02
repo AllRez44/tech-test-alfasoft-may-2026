a0f89bd394943dde6dbf048954253eb8c4521dd7
Allan Resende <allanresendegomes@outlook.com>
2026-05-02 15:20:10 -0300

    Update ContactController - Add CRUD methods using ContactService

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,16 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Contact;
+use App\Http\Requests\StoreContactRequest;
+use App\Service\ContactService;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        $contacts = Contact::all();
+        $contacts = ContactService::index();
         return view('home', [
             'contacts' => $contacts,
         ]);
+    }
+
+    public function storeView()
+    {
+        return view('create');
+    }
+
+    public function store(StoreContactRequest $request)
+    {
+        ContactService::store($request);
+        return redirect()->route('home')->with('success', 'Contact created successfully.');
+    }
+
+    public function show(int $id)
+    {
+        $contact = ContactService::show($id);
+        return view('show', compact('contact'));
+    }
+
+    public function updateView(int $id)
+    {
+        $contact = ContactService::show($id);
+        return view('edit', compact('contact'));
+    }
+
+    public function update(StoreContactRequest $request, int $id)
+    {
+        ContactService::update($request, $id);
+        return redirect()->route('home')->with('success', 'Contact updated successfully.');
+    }
+
+    public function delete(int $id)
+    {
+        ContactService::delete($id);
+        return redirect()->route('home')->with('success', 'Contact deleted successfully.');
     }
 }
