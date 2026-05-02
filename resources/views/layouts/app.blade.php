687b57ff9c581418b6728bef2b8f7f484976e478
Allan Resende <allanresendegomes@outlook.com>
2026-05-02 12:32:26 -0300

    Add app layout template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <header class="bg-gray-800 text-white p-4">
+            <h1 class="text-3xl font-bold p-2">Contact Management Web App</h1>
+        </header>
+        @yield('content')
+    </body>
+</html>
