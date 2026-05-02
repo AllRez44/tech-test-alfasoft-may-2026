3ed8b32bbc4bdb5d8815a433fe18ef42fe31c4fb
Allan Resende <allanresendegomes@outlook.com>
2026-05-02 14:01:55 -0300

    Add contact seeder

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contact;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,36 @@ class ContactSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $contacts = [
+            [
+                'name' => 'John Doe',
+                'email' => 'john.doe@example.com',
+                'contact' => '912345678',
+            ],
+            [
+                'name' => 'Jane Smith',
+                'email' => 'jane.smith@example.com',
+                'contact' => '923456789',
+            ],
+            [
+                'name' => 'Michael Johnson',
+                'email' => 'michael.johnson@example.com',
+                'contact' => '934567890',
+            ],
+            [
+                'name' => 'Emily Brown',
+                'email' => 'emily.brown@example.com',
+                'contact' => '945678901',
+            ],
+            [
+                'name' => 'David Wilson',
+                'email' => 'david.wilson@example.com',
+                'contact' => '956789012',
+            ],
+        ];
+
+        foreach ($contacts as $contact) {
+            Contact::create($contact);
+        }
     }
 }
